e709c3778484417025b1fa32897b199f4f13dcab
Polakrit K <117198477+polakrit-pipi@users.noreply.github.com>
2025-01-18 22:25:59 +0700

    update email

---
+++ backend/index.js
@@ -0,0 +1,31 @@
+//Import lib
+const express = require('express')
+const cors = require('cors')
+const bodyparser = require('body-parser')
+const nodemailer = require("nodemailer");
+
+
+//port
+const port = 8000
+
+//declare app and app.use
+const app = express()
+app.use(cors())
+app.use(bodyparser.json())
+
+
+//root routes
+app.get('/' , (req, res) => {
+    res.send("hello")
+})
+
+//post email with nodemailer
+app.post('/emails' , async (req, res) =>{
+    options = req.body
+
+})
+
+
+app.listen(port , (req ,res) =>{
+    console.log(`run on http://localhost:${port}`);
+})
