6234a3bd61e4ac7271d3188e0c93f03b9dccbe21
nongnutza12 <nongnutza12@gmail.com>
2025-01-20 18:07:01 +0000

    edit navbar

--- hermes/src/app/components/footer.jsx
+++ hermes/src/app/components/footer.jsx
@@ -5,13 +5,13 @@ const Footer = () => {
         <>
             <div className="flex flex-row justify-between  px-[2.5vw] bg-black h-[3vw] items-center">
                 <div>
-                <p className='text-[1vw]'>Hermes-Dev</p>
+                <p className='text-[0.7vw]'>Hermes-Dev</p>
                 </div>
 
                 <div className="flex justify-between flex-row gap-[0.75vw]">
-                <a href=""><img className=' w-[2vw]  ' src="images/Group_62.png" alt="" /></a>
-                <a href=""><img className=' w-[2vw]  ' src="images/Group_63.png" alt="" /></a>
-                <a href=""><img className=' w-[2vw]  ' src="images/Group_64.png" alt="" /></a>
+                <a href=""><img className=' w-[1.4vw]  ' src="images/Group_62.png" alt="" /></a>
+                <a href=""><img className=' w-[1.4vw]  ' src="images/Group_63.png" alt="" /></a>
+                <a href=""><img className=' w-[1.4vw]  ' src="images/Group_64.png" alt="" /></a>
                 </div>                
             </div>
         </>
