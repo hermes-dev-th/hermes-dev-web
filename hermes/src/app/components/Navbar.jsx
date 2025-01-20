83dc496484ae71c15769171e98ae63c39bda157c
Polakrit K <117198477+polakrit-pipi@users.noreply.github.com>
2025-01-21 00:42:58 +0700

    add key prob

--- hermes/src/app/components/Navbar.jsx
+++ hermes/src/app/components/Navbar.jsx
@@ -1,21 +1,31 @@
 export default function Navbar() {
-    return (
+  return (
+    <div>
+      <nav className="flex fixed h-[4vw] w-full bg-white justify-between items-center px-[1.5vw]">
         <div>
-            <nav className="flex fixed h-[4vw] w-full bg-white justify-between items-center px-[1.5vw]">
-        <div>
-            <img src="/material/Hermes-Dev-Logo.png" alt="Logo" className="w-[9vw]" />
+          <img
+            src="/material/Hermes-Dev-Logo.png"
+            alt="Logo"
+            className="w-[9vw]"
+          />
         </div>
         <div>
-            {[
-                ['About Us', '/about'],
-                ['Services', '/services'],
-                ['Pricing', '/pricing'],
-                ['Contact Us', '/contact'],
-            ].map(([title, url]) => (
-                <a href={url} className="px-3 py-2 text-black font-semibold hover:bg-slate-100 hover:text-slate-900 font-Sukhumvit">{title}</a>
-            ))}
+          {[
+            ["About Us", "/about"],
+            ["Services", "/services"],
+            ["Pricing", "/pricing"],
+            ["Contact Us", "/contact"],
+          ].map(([title, url]) => (
+            <a
+              key={title}
+              href={url}
+              className="px-3 py-2 text-black font-semibold hover:bg-slate-100 hover:text-slate-900 font-Sukhumvit"
+            >
+              {title}
+            </a>
+          ))}
         </div>
-    </nav>
+      </nav>
     </div>
-    )
+  );
 }
