fdfaf49a2093aa7126a155b767ba7bfad8dc94b1
nongnutza12 <nongnutza12@gmail.com>
2025-01-20 17:14:07 +0000

    create Navbar component, config font

---
+++ hermes/src/app/component/Navbar.jsx
@@ -0,0 +1,17 @@
+export default function Navbar() {
+    return <nav className="flex fixed h-[4vw] w-full bg-white justify-between items-center px-[1.5vw]">
+        <div>
+            <img src="/material/Hermes-Dev-Logo.png" alt="Logo" className="w-[9vw]" />
+        </div>
+        <div>
+            {[
+                ['About Us', '/about'],
+                ['Services', '/services'],
+                ['Pricing', '/pricing'],
+                ['Contact Us', '/contact'],
+            ].map(([title, url]) => (
+                <a href={url} className="px-3 py-2 text-black font-semibold hover:bg-slate-100 hover:text-slate-900 font-Sukhumvit">{title}</a>
+            ))}
+        </div>
+    </nav>
+}
--- hermes/tailwind.config.mjs
+++ hermes/tailwind.config.mjs
@@ -7,6 +7,9 @@ export default {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        sukhumvit: ['SukhumvitFont', 'sans-serif'],
+      },
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
