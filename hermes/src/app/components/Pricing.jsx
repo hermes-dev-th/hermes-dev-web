d5c366b2e26a6ad93ff1dc31033a76c0629cd6a7
nongnutza12 <nongnutza12@gmail.com>
2025-02-14 19:50:27 +0000

    completed pricingcard and pricing section, make the floading animation when hover

--- hermes/src/app/components/Pricing.jsx
+++ hermes/src/app/components/Pricing.jsx
@@ -1,16 +1,16 @@
 import PricingCard from "./PricingCard";
 export default function Pricing() {
     return (
-        <div className="flex flex-col justify-center items-center w-full h-screen">
+        <div className="flex flex-col justify-center items-center w-full h-screen gap-[3vw]">
             <div className="flex flex-col items-center justify-center">
                 <h1 className="text-[3.5vw] font-bold mb-[-1vw]">Pricing</h1>
                 <p className="text-[0.9vw]">Choose a Plan That Fits Your Needs</p>
             </div>
 
             <div className="flex justify-center gap-[5vw] w-full">
-                <PricingCard Title='Basic Blueprint' Descript='For those who need a simple webpage that is ready to use.' Price='10,000'/>
-                <PricingCard Title='Dynamic Dashboard'/>
-                <PricingCard Title='Ultimate Uptime'/>
+                <PricingCard Title='Basic Blueprint' Descript='For those who need a simple webpage that is ready to use.' Price='10,000' Website={true} Image='./images/Only-Hermes-Dev-Logo.png'/>
+                <PricingCard Title='Dynamic Dashboard' Descript='For those who need a backend system for easy content management.' Price='50,000' Website={true} Backend={true} Maintenance={true} Image='./images/Only-Hermes-Dev-Logo.png'/>
+                <PricingCard Title='Ultimate Uptime' Descript='For those who need a fully-featured website with ongoing maintenance and support.' Price='100,000' Website={true} Backend={true} Dashboard={true} Maintenance={true} Admin={true} Image='./images/Only-Hermes-Dev-Logo.png'/>
             </div>
         </div>
     );
--- hermes/src/app/components/PricingCard.jsx
+++ hermes/src/app/components/PricingCard.jsx
@@ -1,17 +1,48 @@
-export default function PricingCard({ Title, Descript, Price }) {
+import Link from "next/link";
+
+export default function PricingCard({ Title, Descript, Price, Website, Backend, Dashboard, Maintenance, Admin, Image}) {
     return (
         <>
-            <div className="border-[0.2vw] border-black rounded-[1vw] w-[20vw]">
-                <div className="flex flex-col my-[2vw]">
+            <Link href="/">
+            <div className="border-[0.2vw] border-black rounded-[1vw] w-[20vw] shadow-xl hover:shadow-2xl hover:scale-105 transition-all duration-300">
+                <div className="flex flex-col my-[1vw]">
+
                     <div className="flex flex-col items-center justify-center mb-[1vw]">
                         <h1 className="text-[1.4vw] font-bold">{Title}</h1>
-                        <p className="text-[0.9vw] text-pretty text-center mx-[1.8vw]">{Descript}</p>
+                        <p className="text-[0.85vw] text-pretty text-center mx-[1.8vw] font-light">{Descript}</p>
                     </div>
-                    <div className="flex flex-col justify-start">
+
+                    <div className="flex flex-col justify-start ml-[1vw] gap-[0.3vw]">
                         <h1 className="text-[1.7vw] font-bold">THB {Price}</h1>
+                        <div className="flex gap-[0.5vw]">
+                            <img src={Website ? './images/check-icon.png' : './images/cross-icon.png'} className="h-[1vw]"></img>
+                            <p className="text-[0.9vw] font-light">Website</p>
+                        </div>
+                        <div className="flex gap-[0.5vw]">
+                            <img src={Backend ? './images/check-icon.png' : './images/cross-icon.png'} className="h-[1vw]"></img>
+                            <p className="text-[0.9vw] font-light">Backend</p>
+                        </div>
+                        <div className="flex gap-[0.5vw]">
+                            <img src={Maintenance ? './images/check-icon.png' : './images/cross-icon.png'} className="h-[1vw]"></img>
+                            <p className="text-[0.9vw] font-light">Maintenance</p>
+                        </div>
+                        <div className="flex gap-[0.5vw]">
+                            <img src={Dashboard ? './images/check-icon.png' : './images/cross-icon.png'} className="h-[1vw]"></img>
+                            <p className="text-[0.9vw] font-light">Dashboard</p>
+                        </div>
+                        <div className="flex gap-[0.5vw]">
+                            <img src={Admin ? './images/check-icon.png' : './images/cross-icon.png'} className="h-[1vw]"></img>
+                            <p className="text-[0.9vw] font-light">Admin</p>
+                        </div>
                     </div>
+
+                    <div className="flex items-center justify-center w-full mt-[1.5vw]">
+                    <img src={Image} alt="Image" className="h-[10vw] w-[10vw]"></img>
+                    </div>
+                    
                 </div>
             </div>
+            </Link>
         </>
 
 
